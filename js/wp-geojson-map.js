110b5605168cc3d108618c82c56e91434859986b
yannbug <yann@abc.fr>
2016-11-22 02:43:02 +0100

    chloropleth test

--- js/wp-geojson-map.js
+++ js/wp-geojson-map.js
@@ -264,9 +264,11 @@ function add_markers( geojson, popup_fields, field_names, gray_if_no, map_type )
 						var ffp = feature.properties['res.F. Fillon']/feature.properties['res.Exprimés'];
 						var shade = (ffp*100-30)*11.7;
 						shade = parseInt( shade );
-						invshade = parseInt( (256-shade) );
+						invshade = parseInt( (256-shade) ); 
 						shade = shade.toString(16);
 						invshade = invshade.toString(16);
+						while (invshade.length < 2)
+							invshade = '0' + invshade;
 						shade = '#' +invshade + invshade + 'ff';
 						layer.setStyle({fillColor: shade, fillOpacity: 0.8});
 						console.log( 'shade:' + shade );
