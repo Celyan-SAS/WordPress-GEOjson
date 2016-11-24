7d1ddbe3723f15944fea7f631797483340c47bbd
yannbug <yann@abc.fr>
2016-11-24 01:30:07 +0100

    infowindow popup_fields

--- js/wp-geojson-map.js
+++ js/wp-geojson-map.js
@@ -393,7 +393,7 @@ function ggmap_init() {
 		
 		// Set mouseover event for each feature.
 		map.data.addListener('mouseover', function(event) {
-			list_highlight( event.feature.ID );
+			list_highlight( event.feature.getId() );
 			console.log( 'highlighting:' + event.feature.ID );
 		});
 		
