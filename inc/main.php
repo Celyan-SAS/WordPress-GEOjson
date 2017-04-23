f18a6ea6e1e53fcae4c5225d4bfa66c076b7086f
Thomas Benlevi <thomas.benlevi@gmail.com>
2017-04-23 22:41:31 +0200

    Better tables

--- inc/main.php
+++ inc/main.php
@@ -543,7 +543,8 @@ class wpGEOjson {
 			$display = $field;
 			if( preg_match( '/^.+\%.+$/', $field ) ) {
 				$fielda = explode( '%', $field );
-				$display = $fielda[0] . '&nbsp;(&nbsp;%&nbsp;' . $fielda[1] . '&nbsp;)';
+				//$display = $fielda[0] . '&nbsp;(&nbsp;%&nbsp;' . $fielda[1] . '&nbsp;)';
+				$display = $fielda[0] . ' (' . $fielda[1] . ')';
 			}
 			
 			$html .= '<td>' . $display . '</td>';
@@ -579,7 +580,8 @@ class wpGEOjson {
 											$v1 = $feature->properties->$field;
 											$v2 = $feature->properties->{$fielda[1]};
 											$p = ( floor( ( $v1 * 1000 / $v2 ) + 0.5 ) / 10 );
-											$html .= '&nbsp;(&nbsp;' . $p . '%&nbsp;)'; 
+											//$html .= '&nbsp;(&nbsp;' . $p . '%&nbsp;)'; 
+											$html .= ' (' . $p . ')'; 
 										}
 										
 										$html .= '</td>';
