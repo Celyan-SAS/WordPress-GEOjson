e14a80a61cb9cf1e3473b7110bb814c46d774e5d
yannbug <yann@abc.fr>
2017-04-24 04:01:34 +0200

    Better tables

--- inc/main.php
+++ inc/main.php
@@ -564,7 +564,7 @@ class wpGEOjson {
 									}
 									
 									if( isset( $feature->properties->$field ) ) {
-										$html .= '<td data-th="' . $field . '">' . $feature->properties->$field; 
+										$html .= '<td data-th="' . preg_replace( '/^res\./', '', $field ) . '">' . $feature->properties->$field; 
 										
 										if( 
 											$perc && 
