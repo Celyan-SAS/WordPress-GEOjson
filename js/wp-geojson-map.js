f6ad327b1579527d61da37e07c96a42ad7591006
yannbug <yann@abc.fr>
2017-11-17 18:40:38 +0100

    Support of marker_icon parameter for leaflet points

--- js/wp-geojson-map.js
+++ js/wp-geojson-map.js
@@ -945,7 +945,7 @@ function add_markers( geojson, params ) {
 							iconUrl: params.big_cluster_icon,
 							iconSize: [32, 32],
 							iconAnchor: [16, 16],
-							popupAnchor: [0, 16]
+							popupAnchor: [0, -16]
 						});	//TODO: don't specify size?
 					if( params.marker_icon )
 						return L.icon({ iconUrl: params.marker_icon });
