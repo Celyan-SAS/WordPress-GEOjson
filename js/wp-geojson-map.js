09ab4f0f30fb0e09ba87c0b4437bf6bfe38ad3c4
Silverio <silver@wpandco.fr>
2019-05-13 13:16:03 +0200

    Clusturing in ggmap

--- js/wp-geojson-map.js
+++ js/wp-geojson-map.js
@@ -1026,20 +1026,17 @@ function add_markers( geojson, params ) {
 		
 		/** This code allows clustering **/
 		if( 'yes' == params.cluster_points ) {	
-			
-console.log(params.marker_icon);
-console.log(params.medium_cluster_icon);
-			
+						
 			var flmarkers = geojson.features.map(function (feature) {
 				var position = new google.maps.LatLng( feature.geometry.coordinates[1], feature.geometry.coordinates[0] );
 				if(typeof params.marker_icon!= 'undefined' &&  params.marker_icon!=''){
-					var marker = new google.maps.Marker({ 'position': position, 'icon':marker_icon });
+					var marker = new google.maps.Marker({ 'position': position, 'icon':params.marker_icon });
 				}else{
 					var marker = new google.maps.Marker({ 'position': position});
 				}
 				return marker;
 			});
-			if(typeof params.medium_cluster_icon!= 'undefined' &&  medium_cluster_icon!=''){
+			if(typeof params.medium_cluster_icon!= 'undefined' &&  params.medium_cluster_icon!=''){
 				var markerCluster = new MarkerClusterer(map, flmarkers,{ imagePath: params.medium_cluster_icon});
 			}else{
 				var markerCluster = new MarkerClusterer(map, flmarkers,{ imagePath: 'https://cdn.rawgit.com/googlemaps/js-marker-clusterer/gh-pages/images/m' });
