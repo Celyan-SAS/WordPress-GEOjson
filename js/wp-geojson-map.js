b44012540f17f7a9e0588e2a0f09c7202f8fec63
Silverio <silver@wpandco.fr>
2019-05-23 12:16:01 +0200

    Centrer map

--- js/wp-geojson-map.js
+++ js/wp-geojson-map.js
@@ -835,7 +835,13 @@ function ggmap_init() {
 		var map_options = '';
 		if( $('#map-canvas').attr('data-map_options') ){
 			map_options = $('#map-canvas').data('map_options');
+			
+				console.log("map options --- ");
+				console.log(map_options);
 			options = get_map_options_object( options, map_options );
+			
+				console.log("final list");
+				console.log(options);
 		}		
 				
 		map = new google.maps.Map(document.getElementById("map-canvas"), options);
