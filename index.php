0391f15d9b84bdcec41db2b1be0b84fcd968e0d9
Silverio <silver@wpandco.fr>
2021-12-16 11:53:41 +0100

    Remplacement du marker icon s'il est déclaré dans shortcode

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /**
  *	@package WordPress GEOjson
  *	@author Celyan
- *	@version 0.0.6
+ *	@version 0.0.7
  */
 /*
  Plugin Name: WordPress GEOjson
  Plugin URI: https://github.com/Celyan-SAS/WordPress-GEOjson
  Description: GEOjson utility library for WordPress
- Version: 0.0.6
+ Version: 0.0.7
  Author: Yann Dubois
  Author URI: http://www.yann.com/
  License: GPL2
