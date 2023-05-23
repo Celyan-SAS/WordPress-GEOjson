57c8fbdcb9ba1aa2d895ed44538a81679a6b8fb8
Yann "Bug" Dubois <yannbug@users.noreply.github.com>
2023-05-23 12:27:36 +0200

    Update index.php
    Bump version number

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 /**
  *	@package WordPress GEOjson
- *	@author Celyan
- *	@version 0.0.7
+ *	@author WP&Co by Celyan
+ *	@version 1.0.0
  */
 /*
  Plugin Name: WordPress GEOjson
  Plugin URI: https://github.com/Celyan-SAS/WordPress-GEOjson
  Description: GEOjson utility library for WordPress
- Version: 0.0.7
- Author: WP&Co
+ Version: 1.0.0
+ Author: WP&Co by Celyan
  Author URI: https://wordpressandco.fr/
  License: GPL2
  */
